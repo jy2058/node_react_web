099a5d1228386517ab6d8d5193d83b7cd5f4c14c
kjy <jydev763@naver.com>
2020-06-06 13:09:17 +0900

    modify

--- db_info.js
+++ db_info.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongodbUri:
-    "mongodb+srv://jykim:<password>@jyweb-4tus5.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    "mongodb+srv://jykim:wldus763@jyweb-4tus5.mongodb.net/test?retryWrites=true&w=majority",
 };
