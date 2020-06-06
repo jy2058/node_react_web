258f272810974d3e4a9ac1d83fb9cb80e4dfa3d8
kjy <jydev763@naver.com>
2020-06-06 23:13:15 +0900

    비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -25,6 +27,25 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+// userSchema save 하기전에 행동 정의
+// next()하면 save하는 곳으로 바로 보냄.
+userSchema.pre("save", function (next) {
+  // this -> userSchema 가르킴
+  var user = this;
+  // 비밀번호 수정할 때만 진행
+  if (user.isModified("password")) {
+    // 비밀번호 암호화
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash; // hash 값으로 저장
+        next();
+      });
+    });
+  }
 });
 
 const User = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 var createError = require("http-errors");
 var express = require("express");
+const bodyParser = require("body-parser");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-
+const { User } = require("./models/User");
 const db_info = require("./db_info");
 const mongoose = require("mongoose");
 
 var app = express();
 
+// DB연결
 mongoose
   .connect(db_info.mongodbUri, {
     useNewUrlParser: true,
@@ -27,13 +28,29 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// application/x-www-form-urlencoded 분석
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json 분석
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+// 회원가입 정보를 DB에 저장
+app.post("/register", (req, res) => {
+  console.log(res.body);
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
