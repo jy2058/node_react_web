624565c63e31e3b0214b51f2e84eaa6661880174
kjy <jydev763@naver.com>
2020-06-07 16:11:11 +0900

    server 폴더 생성 및 경로 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Router, Route, Switch } from "react-";
+// import { Router, Route, Switch } from "react-";
 
 function App() {
   return (
